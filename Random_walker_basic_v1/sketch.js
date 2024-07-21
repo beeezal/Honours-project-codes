a8b0f0e3be874deaaff011366e7a25b87ff18908
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2024-07-21 22:39:50 +0530

    Adding basic comments to first code

--- Random_walker_basic_v1/sketch.js
+++ Random_walker_basic_v1/sketch.js
@@ -1,29 +1,42 @@
+/* A 2D random walk performed by a point object on a screen.
+The Probability distribution taken for the RW is Uniform(-1,1) - separately for the x-value and y-value*/
+
 class Walker {
+  //This is the class for the point object
   constructor() {
+    //we are placing the point at the center of the screen
     this.x = width / 2;
     this.y = height / 2;
   }
 
   show() {
+    //Displays the point at the given location
     stroke(0);
     point(this.x, this.y);
   }
 
   step() {
-    this.x += floor(random(-1, 2));
-    this.y += floor(random(-1, 2));
+    //Changes the location of the point randomly within a unit square
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
   }
 }
 
 let w;
 
+//Global functions inbuilt in p5.js, setup() runs once and draw() runs repeatedly
+/*The setup() function is used to define the initial environment properties such as screen size and background color
+and creating any objects required in the programme*/
 function setup() {
   createCanvas(700, 500);
-  w = new Walker();
+  w = new Walker();   //Creating an object w of the class Walker
   background(255);
 }
 
+/*The draw() function is called repeatedly. It is used to animate the objects in the canvas,
+by repeatedly changing the variables of the objects presented on the screen*/
 function draw() {
+//*Calling the functions of the object w - show() and step() 
   w.show();
   w.step();
 }
