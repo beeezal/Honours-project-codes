dc3ab4b6d5c75f24809984b0a062fc92d5e4f27a
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2024-09-11 11:37:54 +0530

    Adding comments_1
    1st iteration of adding comments to all the files for documentation purposes.
    - Added comments to Random_walker_basic_v1
    - Added comments to Random_walker_Vector

--- Random_walker_Vector/sketch.js
+++ Random_walker_Vector/sketch.js
@@ -1,56 +1,52 @@
-let w;
-
-function setup() {
-    createCanvas(windowWidth,windowHeight);
-    background(200);
-    w = new Walker(width / 2, height / 2, 20);
-}
-
-function draw() {
-  w.display();
-
-  /* Remove below comment slashes, if we need to also vary step size by a custom distribution */
-  //let vel_mag =(frameCount%180==0)?genRandomNum()*50:1;
-  //w.move(vel_mag);
-  w.move(3,true); 
-}
+/*Creating a RW - Random Walker class with vectors.
+p5.Vector implements vectors in p5*/
 
 class Walker{
-  t = 100;
+   /* t - time, in units of frameCount. Used as input of noise(). 100 was chosen arbitrarily as starting point in time
+   t is a Class field - not accesible by instances*/
+
+  //t = 100; 
+  //Uncomment above if RW is controlled by noise                                      
   constructor(x, y, r) {
-    this.location = createVector(x, y);
-    this.v = p5.Vector.random2D();
-    this.r = r;
-    this.sa = random(0,TWO_PI);
+    //Instance made with parameters position (x,y) and radius - r
+    this.location = createVector(x, y);         //createVector() instantiates a vector object
+    this.velocity = p5.Vector.random2D();              
+    this.sa = random(0,TWO_PI);                 //sa - step angle
   }
   
   display() {
     stroke(0);
     fill(100);
+    // Displaying the step of the walker using a relatively thin line
     strokeWeight(this.r/1.5);
-    line(this.location.x - this.v.x, this.location.y - this.v.y,this.location.x, this.location.y);
+    line(this.location.x - this.velocity.x, this.location.y - this.velocity.y,this.location.x, this.location.y);
+    //Displaying the walker as a circle of radius r
     strokeWeight(1);
     circle(this.location.x, this.location.y, this.r);
   }
 	
   
   move(vel_mag=1,chk_edges=false) {
+    /* Idea used is new location = old location + velocity vector
+    Velocity vectors changes every 3 frame randomly changing the params step size (vel_mag) and/or direction (sa) 
+    3 frames picked as a standard accross all walker methods*/
     if (frameCount%3==0){
       this.sa = random(0,TWO_PI);
+      //Uncomment below if RW is controlled by noise
+      //this.sa = noise(this.t)*TWO_PI;
     }
-    //Uncomment below if doing a random walk with noise 
-    //this.sa = noise(this.t)*TWO_PI;
-    this.v.set(cos(this.sa), sin(this.sa));
-    this.v.mult(vel_mag);
-    this.location.add(this.v);
+    this.velocity.set(cos(this.sa), sin(this.sa));
+    this.velocity.mult(vel_mag);
+    this.location.add(this.velocity);
 		if (chk_edges){
 			this.checkEdges();
 		}
-    //Uncomment below if doing a random walk with noise
-    this.t += 0.01;
+    //Uncomment below if RW is controlled by noise
+    //this.t += 0.01;
   }
 	
   checkEdges() {
+    //Function to check if the walker has crossed the canvas edges, if so - wrap around
     if (this.location.x > width+this.r) {
       this.location.x = this.location.x - (width + this.r);
     } else if (this.location.x < -this.r) {
@@ -65,11 +61,13 @@ class Walker{
 }
 
 function windowResized(){
+  //Function to resize canvas when window is resized - in other words, resize our sketch when windown is resized
   resizeCanvas(windowWidth,windowHeight);
   background(200);
 }
 
-function genRandomNum(){
+function RandomSquared(){
+  //Function to generate random numbers with a x^2 distribution - using accept reject method
   let x = random();
   let y = random();
   while (y>x**2){
@@ -77,4 +75,20 @@ function genRandomNum(){
     y = random();
   }
   return x;
+}
+
+let w;
+
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+    background(200);
+    w = new Walker(width / 2, height / 2, 20);
+}
+
+function draw() {
+  w.display();
+  /* Remove below comment slashes, if we need to also vary step size by a custom distribution */
+  //let vel_mag =(frameCount%180==0)?RandomSquared()*50:1;
+  //w.move(vel_mag);
+  w.move(3,true); 
 }
--- Random_walker_basic_v1/sketch.js
+++ Random_walker_basic_v1/sketch.js
@@ -1,22 +1,22 @@
-/* A 2D random walk performed by a point object on a screen.
+/* A 2D random walk performed by a point object on a screen (Random Walker)
 The Probability distribution taken for the RW is Uniform(-1,1) - separately for the x-value and y-value*/
 
 class Walker {
-  //This is the class for the point object
+  //This is the class for the RW
   constructor() {
-    //we are placing the point at the center of the screen
+    //we are placing the walker at the center of the screen
     this.x = width / 2;
     this.y = height / 2;
   }
 
   show() {
-    //Displays the point at the given location
+    //Displays the walker at the given location
     stroke(0);
     point(this.x, this.y);
   }
 
   step() {
-    //Changes the location of the point randomly within a unit square of the current location
+    //Changes the location of the walker randomly within a unit square of the current location
     this.x += random(-1, 1);
     this.y += random(-1, 1);
   }
@@ -29,14 +29,14 @@ let w;
 and creating any objects required in the programme*/
 function setup() {
   createCanvas(700, 500);
-  w = new Walker();   //Creating an object w of the class Walker
+  //Creating an instance 'w' of the of the class Walker
+  w = new Walker();   
   background(255);
 }
 
 /*The draw() function is called repeatedly in a infinte loop. It is used to animate the objects in the canvas,
-by repeatedly changing the variables of the objects presented on the screen*/
-function draw() {
-//*Calling the functions of the walker object 'w' - show() and step() 
+by repeatedly changing the properties of the object every frame*/
+function draw() { 
   w.show();
   w.step();
 }
