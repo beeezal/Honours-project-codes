87463f6262bfbb009272ed95588bc9b325cafe4f
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2025-02-07 22:30:50 +0530

    feat!: Add trial path to WalkerNormal and refactor it's code
    - Make it comparable to AutonWalker, hence adding the trial path functionality
    - Add a setAngle() function to make step() look cleaner, and to see commanality throughout movers
    - change checkEdges() to be more visually realistic transition from edge to edge
    - Add a posHistory list, updateHistory() to add and maintain history of positions
    - Add code to display() to draw the trial path
    - Add parameter drawConnectingLines to display() to toggle drawing of path lines when background is not refreshed.

--- Normal_Random_walker/sketch.js
+++ Normal_Random_walker/sketch.js
@@ -10,51 +10,84 @@ class WalkerNormal {
 
     this.directionAngle = this.vel.heading();   
 		this.col = color(100)												//col - initialised with the grayscale value 100
+
+    this.posHistory = [];
+    this.showHistory = true;
   }
   
-  display() {
-		stroke(0);
-    fill(this.col);
+  display(drawConnectingLines = false) {
+    // Loop through posHistory and draw lines between consecutive points
+    if (this.showHistory) {
+      for (let i = 0; i < this.posHistory.length-1; i++) {
+          let prevPos = this.posHistory[i];
+          let currPos = this.posHistory[i + 1];
+
+          let posChange = p5.Vector.dist(prevPos, currPos);
+          if (posChange >= windowWidth || posChange >= windowHeight) {
+              continue;
+          }
+          line(prevPos.x, prevPos.y, currPos.x, currPos.y);
+      }
+    }
+
     // Displaying the step of the walker using a relatively thin line
-    strokeWeight(this.r/1.5);
-    line(this.pos.x - this.vel.x, this.pos.y - this.vel.y,this.pos.x, this.pos.y);
+    fill(this.col);
+    if(drawConnectingLines){
+      strokeWeight(this.r/1.5);
+      line(this.pos.x - this.vel.x, this.pos.y - this.vel.y,this.pos.x, this.pos.y);
+      strokeWeight(1);
+    }
     // Displaying the walker as a circle of radius r
-    strokeWeight(1);
     circle(this.pos.x, this.pos.y, this.r*2);
   }
   
   step(vel_mag=1,chk_edges=false) {
-    if (frameCount % 3 === 0) {
-			/*randomGaussian() returns a random sample from a N(0,1) 
-			Therefore by performing scaling and translation of the distribution we have 
-			the directionAngle distributed according to N(current direction,π/8)*/
-			
-      this.directionAngle = randomGaussian() * PI / 8 + this.vel.heading();
-			//σ and μ were picked experimentally
-			
-      if (abs(this.directionAngle - this.vel.heading()) >= QUARTER_PI) {       //  Changing color if change in direction is more than 2 S.Ds (π/4 = 2σ) 
-        this.col = color(random(255), random(255), random(255));				       // color is set by choosing random RGB values
-      }
-    }
+    this.updateHistory();
+
+    this.setAngle();
     this.vel.setHeading(this.directionAngle);        // v.setHeading(direction) is same as v = v.mag() * (cos(directionAngle), sin(directionAngle))                                       
-    this.vel.mult(vel_mag);
+    this.vel.setMag(vel_mag);
     this.pos.add(this.vel);
-		if (chk_edges){
-			this.checkEdges();
-		}
+
+		if (chk_edges){this.checkEdges();}
   }
   
+  setAngle(sigma = PI/8, mu = this.vel.heading(), changeRate = 3, changeColor = true){ 
+    // Function to set the direction angle of the walker's velocity
+    // changeRate - rate at which we change the angle in terms of frames
+    // changeColor - Whether walker's color changes when the direction changes significantly (>2 SDs)
+
+    if (frameCount % changeRate === 0) {
+			// randomGaussian() returns a random sample from a N(0,1) 
+			// Therefore by performing scaling and translation of the distribution we have 
+			// the directionAngle distributed according to N(current direction,π/8) or N(μ,σ)
+			
+      this.directionAngle = randomGaussian() * sigma + mu;        //σ and μ were picked experimentally
+			
+        // Changing color if change in direction is more than 2 SDs
+      if (abs(this.directionAngle - mu) >= 2*sigma && (changeColor)) {
+      this.col = color(random(255), random(255), random(255));        // color is set by choosing random RGB values
+      }
+    }
+  }
+
+  updateHistory(){
+    this.posHistory.push(this.pos.copy());
+    if (this.posHistory.length > 500) { 
+        this.posHistory.shift();
+    }
+  }
   checkEdges() {
 		//Function to check if the walker has crossed the canvas edges, if so - wrap around
-    if (this.pos.x > width) {
-      this.pos.x = 0;
-    } else if (this.pos.x < 0) {
-      this.pos.x = width;
+    if (this.pos.x > width + this.r) {
+      this.pos.x = this.pos.x - (width + this.r);
+    } else if (this.pos.x < -this.r) {
+        this.pos.x = width + (this.pos.x + this.r);
     }
-    if (this.pos.y > height) {
-      this.pos.y = 0;
-    } else if (this.pos.y < 0) {
-      this.pos.y = height;
+    if (this.pos.y > height + this.r) {
+        this.pos.y = this.pos.y - (height + this.r);
+    } else if (this.pos.y < -this.r) {
+        this.pos.y = height + (this.pos.y + this.r);
     }
   }
 }
@@ -67,18 +100,14 @@ function windowResized(){
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  background(200);
 	
 	//Initialising in the centre of the screen with size 20 is standard. Can be played around with.
   w = new WalkerNormal(width / 2, height / 2, 10);
-  background(200);
 }
 
 function draw() {
+  background(200);
   w.display();
-  w.step(3,true);     // vel_mag = 3 is just an arbitrary standard picked after experimentation
+  w.step(/*vel_mag*/ 3,/*chk_edges*/ true);     // vel_mag = 3 is just an arbitrary standard picked after experimentation
 }
-
-/* 
-3. add posHistory. 
-4. add a setAngle function and parameters for - σ and μ - but default to π/8, curr direction
-*/
