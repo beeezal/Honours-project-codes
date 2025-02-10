1352bb459bc884f9ec2977c82958dc8c7af0a521
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2025-02-10 21:08:33 +0530

    refactor: Change mechanics of PerlinWalker to be comparable to other RW
    - Remove use of separated x, y coords and use vectors to manipulate pos
    - Remove direct manipulation of location as part of the PerlinWalker
    - Change how step() works to fit the regular template of RWs - Wanderer and NormalWalker
    
    NOTE: Next commit changes to be made are mentioned in the end of Random_walker_PerlinNoise/sketch.js

--- Random_walker_PerlinNoise/sketch.js
+++ Random_walker_PerlinNoise/sketch.js
@@ -2,7 +2,7 @@
 Perlin noise is used in different ways to create different effects
 Hereafter noise value will be referred as n-value*/
 
-class Walker {
+class PerlinWalker {
   // Setting class fields - offests for each co-ordinate (starting point in time for the noise function)
   // x and y offests are always increased and z offest only when step() affecting_parameter is 'step'
   xoff = 0;
@@ -11,50 +11,44 @@ class Walker {
    // Note that the difference in exact values of x, y, z offsets is arbitrary, 
    // but the difference itself is important, in order to avoid co-orrelation between each direction (at a given time t)
   constructor(x,y,r) {
-    this.x = x;
-    this.y = y;
+    this.pos = createVector(x,y);   
+    this.vel =p5Vector.random2D();  
+
     this.r = r;
+
+    this.posHistory = [];
+    this.showHistory = true;
   }
 
   display() {
     // Displays the walker at the current location with a circle of radius r
     stroke(0);
     fill(100);
-    circle(this.x, this.y,this.r);
+    circle(this.pos.x, this.pos.y, this.r*2);    //last param of circle() is diameter
   }
 
-  step(affecting_params='direction') {
-    // Switch statement to determine the which parameter is being affected. Default: direction
-    switch(affecting_params){
-      // Changing the direction in which the walker is moving
-      case 'direction':
-        // maping n-value of change in x, y coords from (0,1) to (-1,1) 
-        // This enables RW to move in all directions
-        this.x += map(noise(this.xoff),0,1,-1,1)*3;     
-        this.y += map(noise(this.yoff),0,1,-1,1)*3;     //3 is the maginitude of the step. An arbitrary standard accross all RW methods
-        break;
-      // Directly changing the location of the walker
-      case 'location':
-        this.x = noise(this.xoff)*width;                //noise(t)*k - scales n-value to (0,k) from (0,1)
-        this.y = noise(this.yoff)*height;               //Therefore here x-values are (0,width) and y-values are (0,height)
-        break;
-      
-      // Changing both step size and direction
-      case 'step':
-        let vel_mag = noise(this.zoff)*(this.r*0.75);
-        this.x += map(noise(this.xoff),0,1,-1,1)*vel_mag;
-        this.y += map(noise(this.yoff),0,1,-1,1)*vel_mag;
+  step(noisyStepSize = false, vel_mag = 3, relativeMaxStepSize = 0.75) {
+    if(noisyStepSize){
+      // Changing step size according to perlin noise, where zoff is the input for noise() and
+      // the output is mapped from (0,1) to (0,r*0.75) by default - experimentally observed to be visually appealing
+        vel_mag = noise(this.zoff)*(this.r*relativeMaxStepSize);
         this.zoff += 0.01;
-        break;
-    }
-    
+    }   
+    this.noisyVelocity(this.xoff, this.yoff);
+    this.vel.mult(vel_mag);
+    this.pos.add(this.vel);
+
     this.xoff += 0.01;
     this.yoff += 0.01;
+  }
 
-    // NOTE: the affecting parameters' names are a bit misleading
-    // They do not actually in all cases directly refer to the parameter being affected. 
-    // But rather refer to the methods previously used to affect such parameters (with noise).
-    // For EG: all modes affect step_size to some extent. But none explicitly other than 'step'
+  static noisyVelocity(xOffset, yOffset){
+    // Changing the direction in which the walker is moving
+    // by providing a n-value of change at each frame for x, y coords of this.vel
+    // The n-value of change is mapped from (0,1) to (-1,1) which enables RW to move in all directions
+
+    this.vel.set(map(noise(xOffset),0,1,-1,1), 
+                 map(noise(yOffset),0,1,-1,1));
   }
   
 }
@@ -64,10 +58,17 @@ let w;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
-  w = new Walker(width/2, height/2, 20);   //Creating an object w of the class Walker
+  w = new PerlinWalker(width/2, height/2, 10);   //Creating an object w of the class Walker
 }
 
 function draw() {
-  w.display();                             //Be aware of which function you call first according to the step type you chose
-  w.step('step');
+  w.display();
+  w.step(/*noisyStepSize*/ /*vel_mag*/ /*relativeMaxStepSize*/);
 }
+
+/* 
+1. Mention the real use of vel_mag how it is different from other walkers.
+2. Also ellaborate on how the difference in how the randomness is introduced and use is different
+3. Add params chk_edges and showHistory, explain all the existing params concisely
+4. Add the trialing behaviour
+*/
