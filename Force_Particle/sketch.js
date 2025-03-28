6c7e33c43ed1b5b663f18b734e07a16f160a06ce
Bezal Stephen <bezal.stephen22ug@apu.edu.in>
2025-03-28 22:36:04 +0530

    chore: update variable names and comment positions
    Change variable names and move and edit comments to:
    - avoid explaining trival details and syntax.
    - to fit the thesis flow better.
    - adhereing to style used in all other code.
    Change declaration of global variables adhere to style / code principles followed

--- Force_Particle/sketch.js
+++ Force_Particle/sketch.js
@@ -1,106 +1,99 @@
-//Code to simulate a bouncing ball under the influence of gravity and frictional forces
+// Program to simulate a bouncing ball under the influence of gravity and frictional forces
+class Mover {
+  /* This is the syntax for creating a class, now within this block 
+  we can define and attach properties and functions (methods) to the class
+  by using the 'this' keyword */
 
-// Initialising the mover object, gravitational acceleration and coeffecient of friction
-// Uses of all are explained in the code
-let mv;
-let gravity; 
-let fric_coeff = 0.075;     // An arbitrary choice made after some experimentation
+  // The constructor sets the initial values that are required after instantiation
+  // for the Mover class we require, x and y coordinates, and mass of the object
 
-class Mover{
-  /* This class contains functionality that allows us to mainpulate an object through environmental forces */
+  constructor(x, y, m) {
+    // The velocity chosen such that it showcases all the features of the program
 
-  constructor(x,y,m){
-    this.location = createVector(x,y);
-    this.velocity = createVector(2,-1);  //Arbitrary choice that showcases all the features of the code
-    this.acceleration = createVector(); // Creates a vector of default values (0,0)
+    this.pos = createVector(x, y);
+    this.vel = createVector(2, -3);
+    this.acc = createVector();    // Creates a vector of default values (0,0)
     this.mass = m;
-    this.r = m*1.75;                    // Object radius ∝ mass - bigger ⇒ heavier
+    this.r = m * 0.875;   // Object radius ∝ mass - i.e., assuming bigger ⇒ heavier
+    this.D = this.r * 2;    // Diameter - used as argument for drawing circle
   }
-  
-  update(){
-    /* Updates the location of the object, according to the forces acting on it at frame 't'
-    
-    First  the acceleration is updated according to the net force accting on the mover
-    using the formula a = F/m. Check the applyforce() function for implementation. 
-    
-    Then the acceleration is added to the current velocity to get new velocity. 
-    Refactor the discrete vector equation [a = (v₁ - v₀)/t] to understand why we add (t=1 here)
-    Similarly the new velocity is added to the location to get the new location of the mover */
-    
-    this.velocity.add(this.acceleration);
-    this.location.add(this.velocity);
-    this.acceleration.mult(0);       //Acceleration is reset - only affected by force(s) at that frame
-    
-    /* Note that acceleration is reset before call to bounceEdges() 
-    As we want the effect frictional force applied in bounceEdges() to be carried over to the next frame */
+
+  // Inside classes, function/methods don't need the 'function' keyword,
+  update() {
+    // The below three lines implement the physical laws as sequenced in the modelling section
+    // This implementation will be used throughout all simulations as part of the update() method
+
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);       //Acceleration is reset - only affected by force(s) at that frame
+
+    // bounceEdges() is called after the reset, since the effect of friction
+    // applied in bounceEdges() is only used in the next frame 
     this.bounceEdges();
   }
-  
-  show(){
-    //Draws a circle at the current location (x,y) with radius r
-    circle(this.location.x,this.location.y,this.r);
+
+  show() {
+    circle(this.pos.x, this.pos.y, this.D);
   }
-  
-  applyForce(force){
-    //Making a copy of the given force and dividing by movers' mass
-    let f = p5.Vector.div(force, this.mass); 
-    this.acceleration.add(f);      //Adding so that effect of net force will be considered each frame
+
+  applyForce(force) {
+    // Since gravitational acceleration is constant, we have to create a copy
+    // And apply second law to the copied vector
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
   }
-  
-  bounceEdges(fric_Force=true){    
-    /* Function to create a bouncing appearence of the mover when it contacts an edge
-    
-    Checks if mover is located beyond an edge 
-    if so reset the location to the edge and reflect the velocity along the edge axis */
-    
-    if (this.location.y + this.r/2 > height){
-      this.velocity.y*=-0.85;
-      this.location.y += height - (this.location.y+this.r/2);
-      
-      /* Now if we have frictional surface as the bottom edge,
-      then after the reset a frictional force is applied */
-      if (fric_Force){
-        /* Formula used is fric = µN (-û) where 
-        µ - frictional coeffecient, N- Normal force, û - unit vector along current velocity 
-        
-        Basically friction is a force acting along the opposite direction of the velocity */ 
-        let fric = p5.Vector.setMag(this.velocity, fric_coeff); //Here we consider N = 1
+
+  bounceEdges(fric_Force = true) {
+    if (this.pos.y + this.r > height) {
+      this.vel.y *= -0.85;
+      this.pos.y += height - (this.pos.y + this.r);
+
+      if (fric_Force) {
+        // Formula used is fric = µN (-û) where 
+        // µ - frictional coeffecient, N- Normal force, û - unit vector along current velocity 
+        // Basically friction is a force acting along the opposite direction of the velocity
+
+        let fric = p5.Vector.setMag(this.vel, fric_coeff); 
         fric.mult(-1);
         this.applyForce(fric);
       }
-      
-      
-    } else if (this.location.y - this.r/2 < 0){
-      this.velocity.y*=-0.85;
-      this.location.y -= this.location.y - this.r/2
+    } else if (this.pos.y - this.r < 0) {
+      this.vel.y *= -0.85;
+      this.pos.y -= this.pos.y - this.r
     }
-    
-    if (this.location.x + this.r/2 > width){
-      this.velocity.x*=-0.95;
-      this.location.x += width - (this.location.x+this.r/2);
-    } else if (this.location.x - this.r/2 < 0) {
-      this.velocity.x*=-0.95;
-      this.location.x -= this.location.x - this.r/2;
+
+    if (this.pos.x + this.r > width) {
+      this.vel.x *= -0.95;
+      this.pos.x += width - (this.pos.x + this.r);
+    } else if (this.pos.x - this.r < 0) {
+      this.vel.x *= -0.95;
+      this.pos.x -= this.pos.x - this.r;
     }
   }
 }
 
+let mv;   // The mover instance decleration
+let gravity;
+const fric_coeff = 0.075;
+
 function setup() {
-  createCanvas(400,400);
+  createCanvas(400, 400);
   fill(100);
-  gravity = createVector(0,0.075);  // An arbitrary choice made after some experimentation
-  mv = new Mover(200,50,20);
+  gravity = createVector(0, 0.1);
+  mv = new Mover(200, 80, 20);
 
 }
 
 function draw() {
   background(220);
   mv.show();
-  
-  mv.applyForce(p5.Vector.mult(gravity,mv.mass));   
-  // Applying any universal forces in the simulation - in this case gravitational force (G)
-  // Note that G is scaled according to mass as the vector 'gravity' refers to graivtational acceleration
-  
+
+  // Applying any universal forces in the simulation - in this case just gravity
+  // But since grativational force actually scales according to mass, we pass a new vector at each frame
+  // i.e., our constant gravity vector multiplied by the mass of the mover
+  mv.applyForce(p5.Vector.mult(gravity, mv.mass));
+
+
   mv.update();
-  
+
 }
